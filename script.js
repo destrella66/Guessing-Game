d7db34ee9769d91b6234fb8f3a6768cd55045af7
Davi Estrella <daviestrella66@gmail.com>
2023-09-02 20:16:33 -0300

    Adciona todos os arquivos e estilos

---
+++ script.js
@@ -0,0 +1,24 @@
+let answer = Math.floor(Math.random() * 10 + 1)
+
+let guesses = 0; 
+
+document.getElementById("submitButton").onclick = function(){
+    let guess = document.getElementById("guessField").value;
+    guess = Number(guess);
+    guesses+=1; 
+    if(guess == answer){
+        alert(`${answer} is the number.  It took you ${guesses} guesses.`)
+        guesses = 0; 
+        answer = Math.floor(Math.random() * 10 + 1); 
+    }
+    else if(guess <= 0){
+        alert("Please, make sure to pick a number between 1 - 10!")
+    }
+    else if(guess < answer){
+        alert("Too small")
+        
+    }
+    else{
+        alert("Too large!")
+    }
+}
